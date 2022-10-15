85e7409d73afc478ad531ca1949598762d3fc52f
ooo3345sjh <ooo3345sjh@gmail.com>
2022-10-15 23:42:07 +0900

    2022/10/15 Book 최종 실습예제 메인메뉴 완료

--- Book/MyStudy/prac/연습용/js/common.js
+++ Book/MyStudy/prac/연습용/js/common.js
@@ -15,7 +15,7 @@
         sc_w2 = scrollShowHide("scroll"),
         sc_w3 = sc_w1 - sc_w2;
 
-    if(sc_w3 > 0){
+    if(sc_w3 >= 0){
         deviceSize.pc -= sc_w3; 
         deviceSize.tablet -= sc_w3; 
         deviceSize.mobile -= sc_w3; 
@@ -45,27 +45,49 @@
     $(function(){
         $(win).trigger('resize');
 
-        $('.pc .menu > .menu2 > ul > li > a').on('mouseover focus', gnbplay);
-        $('.tablet .menu > .menu2 > ul > li > a').on('mouseover focus', gnbplay);
+        $(document).on('mouseover focus', 
+        '.pc .menu > .menu2 > ul > li > a', gnbplay);
+        $(document).on('mouseover focus', 
+        '.tablet .menu > .menu2 > ul > li > a', gnbplay);
+        $(document).on('click', 
+        '.mobile .menu > .menu2 > ul > li > a', gnbplay);
+
         
         function gnbplay(){
             var $ts = $(this);
             var $html = $('html');
             if($html.hasClass('mobile')){
-                
+                $('.menu > .menu2 > ul ul:visible').slideUp(500);
+                $('.menu > .menu2 > ul > li > a').removeClass("on"); 
+                if($ts.next().is(':hidden')){
+                    $ts.addClass("on"); 
+                    $ts.next().stop(true,true).slideDown(500);
+                }
             }
             else {
-                $('.menu > .menu2 > ul ul:visible').slideUp(400);
-                $ts.next().stop(true,true).slideDown(400);
+                $('.menu > .menu2 > ul ul:visible').slideUp(500);
+                $ts.next().stop(true,true).slideDown(500);
             }
-        }
-        
-        $('.pc .menu > .menu2 > ul > li > a').on('mouseleave blur', gnbleave);
-        $('.tablet .menu > .menu2 > ul > li > a').on('mouseleave blur', gnbleave);
+        };
 
+        $(document).on('mouseleave','.pc .menu > .menu2 > ul > li > a',gnbleave);
+        $(document).on('mouseleave','.tablet .menu > .menu2 > ul > li > a',gnbleave);
+        
         function gnbleave(){
             $(".menu > .menu2 > ul ul:visible").slideUp(500);
         }
+        
+        $('.mobile-menu-open button').on('click', function(){
+            $("header > .menu").animate({'left':'0'}, 300);
+            scrollShowHide("hidden");
+        });
+        $('.mobile-menu-close button').on('click', function(){
+            $("header > .menu").animate({'left':'-1000px'}, 300);
+            gnbleave();
+            $('.menu > .menu2 > ul > li > a').removeClass("on");
+            scrollShowHide("scroll");
+        });
+
 
     })
 })(window, jQuery)
